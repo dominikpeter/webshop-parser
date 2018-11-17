78a9501f20e1c83e24c941c9503354e14fad0a41
Dominik Peter <dominik.peter87@gmail.com>
2018-11-17 23:16:14 +0100

    obi rules

--- obi/obi/spiders/parse_obi.py
+++ obi/obi/spiders/parse_obi.py
@@ -1,30 +1,36 @@
 # -*- coding: utf-8 -*-
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
+from scrapy.http import Request
 
 
 class ParseObiSpider(CrawlSpider):
     name = 'obi'
-    allowed_domains = ['www.obi.ch']
+    allowed_domains = ['obi.ch']
     start_urls = (
         'https://www.obi.ch/bauen/c/877',
         'https://www.obi.ch/technik/c/876',
         'https://www.obi.ch/kueche/c/875',
         'https://www.obi.ch/bad/c/4159',
-        )
-
-    rules = (
-    Rule(LinkExtractor(allow=('cms/de/shop-content', ),
-        restrict_xpaths=('//*[contains(@class, "item")]',)),
-            follow=True),
-    Rule(LinkExtractor(allow=('de/sortiment', ),
-        restrict_xpaths=('//*[contains(@class, "item")]',)),
-            follow=True),
-    Rule(LinkExtractor(allow=(''),
-        restrict_xpaths=('//*[contains(@class, "product-image")]',)),
-            follow=True, callback = 'parse_page'),
     )
 
+    rules = (
+        Rule(LinkExtractor(allow=('', ),
+                           restrict_xpaths=('//*[contains(@wt_name, "level2")]',)),
+             follow=True),
+        Rule(LinkExtractor(allow=('', ),
+                           restrict_xpaths=('//*[contains(@wt_name, "level3")]',)),
+             follow=True),
+        Rule(LinkExtractor(allow=(''),
+                           restrict_xpaths=('//*[contains(@wt_name, "level4")]',)),
+             follow=True),
+        Rule(LinkExtractor(allow=(''),
+                           restrict_xpaths=('//*[contains(@tm-data, "content.pagination.next-page.link")]',)),
+             follow=True),
+        Rule(LinkExtractor(allow=(''),
+                           restrict_xpaths=('//*[contains(@class, "product-wrapper")]',)),
+             follow=True, callback='parse_page'),
+    )
 
-    def parse(self, response):
-        pass
+    def parse_page(self, response):
+        yield {"url": response.url}
