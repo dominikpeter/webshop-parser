b38d04f2928bc6b2511fc07edf83b59a70e97c92
Dominik Peter <dominik.peter87@gmail.com>
2018-11-17 22:04:12 +0100

    obi

--- hgc/hgc/spiders/parse_hgc.py
+++ hgc/hgc/spiders/parse_hgc.py
@@ -4,114 +4,118 @@ from random import randint
 
 import scrapy
 from scrapy.exceptions import CloseSpider
+from scrapy.selector import Selector
+
 
 from urllib.parse import urljoin, urlencode
 from json.decoder import JSONDecodeError
 from hgc.items import HgcItem
 from scrapy.http import FormRequest
 
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import StaleElementReferenceException
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import ElementNotVisibleException
-from selenium.common.exceptions import WebDriverException
-from selenium.webdriver.chrome.options import Options
-
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium.common.exceptions import TimeoutException
-
-chrome_options = Options()
-chrome_options.add_argument("--headless")
-
-def random_with_N_digits(n):
-    range_start = 10**(n-1)
-    range_end = (10**n)-1
-    return randint(range_start, range_end)
+import requests
+import re
+import time
+import math
 
 
 class HGCSpider(scrapy.Spider):
-    name = "hgc_backup"
+    name = "hgc"
     allowed_domains = ['shop.hgc.ch']
     start_urls = ['https://shop.hgc.ch']
 
     def __init__(self):
-        self.driver = webdriver.Chrome(chrome_options=chrome_options)
-        self.driver.delete_all_cookies()
-        self.driver.get('https://shop.hgc.ch')
-        self.driver.find_element_by_xpath("//*[@class='content']/a").click()
 
-        self.cat_id = self.driver.find_element_by_xpath(
-            "//*[contains(@class, 'ui-state-default')]/a").get_attribute(
-                "data-id")
+        session = requests.Session()
+        resp = session.get('https://shop.hgc.ch')
 
-        cookies = self.driver.get_cookies()
-        self.cookies = {i['name']: i['value'] for i in cookies}
+        self.cookies = session.cookies.get_dict()
         self.headers = {
-            'Accept-Encoding': 'gzip, deflate, br',
-            'Accept-Language': 'de-CH,de-DE;q=0.9,de;q=0.8,en-US;q=0.7,en;q=0.6',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36',
+            'Origin': 'https://shop.hgc.ch',
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
             'Accept': 'application/json, text/javascript, */*; q=0.01',
-            'Referer': 'https://shop.hgc.ch/fis(bD1kZSZjPTAxMA==)/fisesales/search.htm?q=',
             'X-Requested-With': 'XMLHttpRequest',
             'Connection': 'keep-alive',
         }
 
-        self.driver.close()
-        self.i = 0
-        self.post_url = 'https://shop.hgc.ch/FIS(bD1kZSZjPTAxMA==)/FISESALES/details.ws?event=GET_DETAILS&pitcher=search.htm&receiver='
+        params = {"handler": "getusercatalogues",
+                  "lang": "D",
+                  "getusercatalogues": "true"}
+
+        self.url = re.sub("dashboard.*", "search_solr.ws?", resp.url)
+        self.post_url = re.sub("dashboard.*", "details.ws?", resp.url)
+        self.post_url = self.post_url + "event=GET_DETAILS&pitcher=search.htm&receiver="
+        resp = session.get(self.url + urlencode(params),
+                           headers=self.headers)
+        self.cat_id = resp.json()['catalogues'][0]['id']
+        self.rows = 50
+
+        params = {"handler": "search",
+                  "query": "*",
+                  "rows": "50",
+                  "start": "50",
+                  "sort": "score desc",
+                  "fuzzy": "true",
+                  "catalogue_id": str(self.cat_id),
+                  "_": str(self.timestamp())
+                  }
+
+        resp = session.get(self.url + urlencode(params),
+                           headers=self.headers)
+
+        js = resp.json()
+        self.number_of_items = js['response']['numFound']
+
+        self.pages = math.ceil(self.number_of_items / self.rows)
+        print(self.pages)
 
     def parse(self, response):
+        pass
 
+    def tparse(self, response):
+
+        for i in range(self.pages):
+            start = i * self.rows
+            params = {
+                'handler': 'search',
+                'query': '*',
+                'rows': '50',
+                'start': str(start),
+                'sort': 'score desc',
+                'fuzzy': 'true',
+                'catalogue_id': self.cat_id,
+                'fq': '',
+                'qf': '',
+                '_': str(self.timestamp())
+            }
+            url = self.url + urlencode(params)
+
+            yield scrapy.Request(url, headers=self.headers,
+                                 cookies=self.cookies,
+                                 callback=self.parse_matnr)
+
+    def parse_matnr(self, response):
         try:
             js = json.loads(response.text)
-
-            if not js['response']['docs']:
-                raise CloseSpider("No more items")
-
             for i in js['response']['docs']:
                 id = i['matnr']
                 product = HgcItem()
                 product['id'] = id
-                data = {'matnr': id}
+                data = {'matnr': str(id)}
                 yield FormRequest(self.post_url,
-                    callback=self.parse_json,
-                    headers=self.headers, formdata=data,
-                    cookies=self.cookies,
-                    dont_filter = True,
-                    meta = {"product": product})
-            self.i += 50
+                                  callback=self.parse_json,
+                                  headers=self.headers,
+                                  formdata=data,
+                                  cookies=self.cookies,
+                                  meta={"product": product})
         except JSONDecodeError:
-            self.js = None
             self.log("Json Error")
             pass
-
-        params = {
-            'handler': 'search',
-            'query': '*',
-            'rows': '50',
-            'start': str(self.i),
-            'sort': 'score desc',
-            'fuzzy': 'true',
-            'catalogue_id': self.cat_id,
-            'fq': '',
-            'qf': ''#,
-            # '_': str(random_with_N_digits(13))
-        }
-
-        url = "https://shop.hgc.ch/fis(bD1kZSZjPTAxMA==)/fisesales/search_solr.ws?" + urlencode(params)
-        self.log("Batch = {}".format(self.i/50))
-
-
-        yield scrapy.Request(url, headers=self.headers,
-                             cookies=self.cookies, callback=self.parse,
-                             dont_filter=True)
-
 
     def parse_json(self, response):
         product = response.meta['product']
         js = json.loads(response.text)
         product['detail'] = js
-        yield(dict(product))
+        yield dict(product)
+
+    def timestamp(self):
+        return int(round(time.time() * 1000))
